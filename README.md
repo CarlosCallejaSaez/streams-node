db79a0716b5b744815a5db89270930c93e0e3470
CarlosCallejaSaez <carloscallejasez@gmail.com>
2024-05-14 11:01:53 +0200

    v1

---
+++ README.md
@@ -0,0 +1,18 @@
+# Carlos Streams
+
+Esta es una API simple para subir un archivo de texto, procesarlo convirtiendo todas las líneas a mayúsculas y agregando "Carlos Calleja Sáez" al final de cada línea usando streams, y luego descargar el archivo procesado.
+
+
+## Endpoints de la API
+
+### Subir un Archivo
+
+- **URL:** `/upload`
+- **Método:** `POST`
+- **Parámetro:** `file` (archivo de texto a subir)
+
+### Descargar el Archivo Procesado
+
+- **URL:** `/download`
+- **Método:** `GET`
+
---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { Transform } = require('stream');
+
+const app = express();
+const port = 5000;
+
+
+const upload = multer({ dest: 'uploads/' });
+
+
+const transformLine = new Transform({
+  transform(chunk, encoding, callback) {
+    
+    this.push(chunk.toString().toUpperCase() + ' CARLOS CALLEJA SÁEZ\n');
+    callback();
+  }
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  const filePath = req.file.path;
+  const processedFilePath = path.join(__dirname, 'uploads', 'procesado.txt');
+
+  const readableStream = fs.createReadStream(filePath, { encoding: 'utf8' });
+  const writableStream = fs.createWriteStream(processedFilePath);
+
+  const rl = readline.createInterface({
+    input: readableStream
+  });
+
+  rl.on('line', (line) => {
+    transformLine.write(line + '\n');
+  });
+
+  rl.on('close', () => {
+    transformLine.end();
+  });
+
+  transformLine.pipe(writableStream);
+
+  writableStream.on('finish', () => {
+    res.send('Archivo subido y procesado correctamente. Puedes descargar el archivo procesado en /download');
+  });
+
+  writableStream.on('error', (err) => {
+    console.error('Error durante la escritura del archivo:', err);
+    res.status(500).send('Error durante el procesamiento del archivo.');
+  });
+});
+
+app.get('/download', (req, res) => {
+  const processedFilePath = path.join(__dirname, 'uploads', 'procesado.txt');
+  res.download(processedFilePath, 'procesado.txt', (err) => {
+    if (err) {
+      console.error('Error al descargar el archivo:', err);
+      res.status(500).send('Error al descargar el archivo.');
+    }
+  });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
